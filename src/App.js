7927bfbc7621b77280dd867d6c3d62f94311d5ce
Selvatutorials-SELVAKUMAR <selvatutorials105@gmail.com>
2021-04-24 04:10:13 +0530

    Autocomplete-search-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Suggestions from './Components/Suggestions'
+import Tabular from './Components/Tabular'
+import Data from './data.json'
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      data : Data.data,
+      keyword : "",
+      results : [],
+      showsugg : false,
+      showdetails : false
+    }
+  }
+  matchName = (name,keyword) => {
+    let keyLen = keyword.length
+    keyword = keyword.toLowerCase()
+    name = name.toLowerCase().substring(0,keyLen)
+    return name === keyword && keyLen!==0
+  }
+  onSearch = value => {
+    let results = this.state.data.filter(item => this.matchName(item.name,value))
+    this.setState({
+      results : results
+    })
+  }
+  updateField = value =>{
+    this.setState({
+      keyword : value,
+      showsugg : true
+    })
+    this.onSearch(value)
+  }
+  updateText = (name,id) => {
+    let res = this.state.data.filter(item => item.name === name && item.Emp_id === id)
+    this.setState({
+      keyword : name,
+      showsugg:false,
+      results : res
+    })
+  }
+  clear = () => {
+    this.setState({
+      keyword : "",
+      showsugg:false,
+      results : []
+    })
+  }
+  handleClick = () => {
+    this.setState({
+      showdetails : true
+    })
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  closeDetails = () => {
+    this.setState({
+      showdetails : false,
+      results:[],
+      keyword: "",
+      showsugg : false
+    })
+  }
+  render() {
+    return (
+      <div className = "App">
+        {
+          this.state.showdetails ? <Tabular results = {this.state.results} onclick = {this.closeDetails}/> :
+        <div className = "autocomplete-container">
+          <h1 className = "main-head">Employee Details</h1>
+          <br/>
+          <br/>
+          <div className="search-container">
+            <div className = "input">
+              <input className = "search-bar" placeholder = "Search . . ." onChange = {event => this.updateField(event.target.value) } value = {this.state.keyword} />
+              {
+                this.state.keyword.length > 0 ?
+                <div className = "button">
+                  <button onClick = {() => this.clear()}className="clear">x</button>
+                  </div> : null
+              }
+            </div>
+          </div>
+          {
+            this.state.showsugg && this.state.results.length > 0 ? 
+            <div className = "suggestion-container">
+              <Suggestions results = {this.state.results} update = {this.updateText}/>
+              </div> : 
+              this.state.keyword.length >0 && this.state.results.length === 0 ? 
+              <div className = "no-results">
+                <h1>No Results Found</h1><br/>
+                <h4>Try Different Keywords</h4>
+                </div> : null
+          }
+          {
+            this.state.results.length === 1 && this.state.keyword.toLowerCase() === this.state.results[0].name.toLowerCase() ?
+            <div className = "details">
+                <button className = "detailsbutton" onClick = {() => this.handleClick()}>Get Details</button>
+              </div> : null
+          }
+        </div>
+  }
+      </div>
+        
+    )
+  }
 }
-
-export default App;
